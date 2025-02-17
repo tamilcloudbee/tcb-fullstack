9e6dfa8ba353e0348f3eb21385081df691cbd86d
TamilCloudbee <tamilcloudbee@gmail.com>
2025-02-18 04:16:18 +0530

    added db_identifier

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,17 +1,6 @@
-/*
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "${var.resource_prefix}-rds-subnet-group"
-  subnet_ids = [var.private_subnet_id_1,var.private_subnet_id_2]  # Using private subnet 1
-
-  tags = {
-    Name = "${var.resource_prefix}-rds-subnet-group"
-  }
-}
-
-
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "${var.resource_prefix}-rds-subnet-group"
-  subnet_ids = [var.private_subnet_id_1]  # Use just one private subnet
+  subnet_ids = [var.private_subnet_id_1,var.private_subnet_id_2]  # Use just minimum 2 private subnet
 
   tags = {
     Name = "${var.resource_prefix}-rds-subnet-group"
@@ -19,36 +8,7 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
 }
 
 resource "aws_db_instance" "rds-db" {
-  db_name              = var.db_name
-  engine               = "mysql"
-  engine_version       = "8.4.4"
-  instance_class       = "db.t3.micro"
-  storage_type         = "gp2"
-  allocated_storage    = 20
-  vpc_security_group_ids = [var.rds_security_group_id]
-  username             = var.db_admin_user
-  password             = var.db_admin_password
-  publicly_accessible   = false 
-  skip_final_snapshot  = true
-  multi_az               = false
-  tags = {
-    Name = "${var.resource_prefix}-rds"
-  }
-
-
-}
-*/
-
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = "${var.resource_prefix}-rds-subnet-group"
-  subnet_ids = [var.private_subnet_id_1,var.private_subnet_id_2]  # Use just one private subnet
-
-  tags = {
-    Name = "${var.resource_prefix}-rds-subnet-group"
-  }
-}
-
-resource "aws_db_instance" "rds-db" {
+  db_instance_identifier = "${var.resource_prefix}-rds-db"  # Add the DB Identifier
   db_name              = var.db_name
   engine               = "mysql"
   engine_version       = "8.4.4"
