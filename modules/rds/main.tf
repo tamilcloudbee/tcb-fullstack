3aa9cf164298b63c54cc8100aceb0617e40b3ec9
TamilCloudbee <tamilcloudbee@gmail.com>
2025-02-18 03:58:37 +0530

    associated db subnet group name

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
     Name = "${var.resource_prefix}-rds-subnet-group"
   }
 }
-*/
+
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "${var.resource_prefix}-rds-subnet-group"
@@ -36,4 +36,33 @@ resource "aws_db_instance" "rds-db" {
   }
 
 
+}
+*/
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "${var.resource_prefix}-rds-subnet-group"
+  subnet_ids = [var.private_subnet_id_1]  # Use just one private subnet
+
+  tags = {
+    Name = "${var.resource_prefix}-rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "rds-db" {
+  db_name              = var.db_name
+  engine               = "mysql"
+  engine_version       = "8.4.4"
+  instance_class       = "db.t3.micro"
+  storage_type         = "gp2"
+  allocated_storage    = 20
+  vpc_security_group_ids = [var.rds_security_group_id]
+  username             = var.db_admin_user
+  password             = var.db_admin_password
+  publicly_accessible   = false 
+  skip_final_snapshot  = true
+  multi_az               = false
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name  # Associate the subnet group
+  tags = {
+    Name = "${var.resource_prefix}-rds"
+  }
 }
